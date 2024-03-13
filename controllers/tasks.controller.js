a19d20c4fa19b8d3c466c6eed7bf748a81b21a87
paussaanz <paula@sanz.eu>
2024-03-13 20:21:13 +0100

    added nodemailer to tasks

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,22 +1,25 @@
 const Task = require("../models/Task.model");
 const User = require("../models/User.model");
+const {
+    transporter,
+    createEmailTemplate,
+} = require('../config/nodemailer.config');
 
 module.exports.getAllTasks = async (req, res, next) => {
     try {
         const tasks = await Task.find().populate("userId");
-        console.log("populatedTasksssss", tasks)
         res.status(200).json(tasks);
     } catch (error) {
         next(error);
     }
 };
 
-module.exports.addUserToTask = async (req, res) => {
-    const { userId, taskId } = req.params;
+module.exports.addUsersToTask = async (req, res) => {
+    const { taskId } = req.params;
 
     try {
-        const task = await Task.findByIdAndUpdate(taskId, { $push: { userId } }, { new: true }).populate("userId")
-        res.status(200).json(task)
+        const task = await Task.findByIdAndUpdate(taskId, { userId: req.body });
+        res.json(task)
     }
     catch (error) {
         res.status(500).send({ message: error.message || 'An error occurred while adding task to user' });
@@ -52,6 +55,26 @@ exports.updateTaskStatus = async (req, res, next) => {
     try {
         const task = await Task.findByIdAndUpdate(taskId, { $set: { status: true } }, { new: true })
             .populate("userId");
+        console.log('task', task)
+
+         task.userId.forEach((user) => {
+            console.log("Entro")
+            transporter.sendMail({
+                from: process.env.NODEMAILER_EMAIL,
+                to: user.email,
+                subject: `Your task ${task.name} was updated`,
+                html: createEmailTemplate({ name: 'Paula', message : 'sosa' }),
+            })
+                .then(info => {
+                    console.log('Email sent: ' + info.response);
+                })
+                .catch(emailError => {
+                    console.error('Error sending email:', emailError);
+                    throw emailError;
+                });
+
+        })
+
 
         if (!task) {
             return res.status(404).send({ message: 'Task not found' });
@@ -62,36 +85,19 @@ exports.updateTaskStatus = async (req, res, next) => {
         next(error)
     }
 };
-
-// exports.updateTaskStatus = async (req, res, next) => {
-//     const { taskId } = req.params;
-
-//     try {
-//         const task = await Task.findByIdAndUpdate(taskId, { $set: { status: true } }, { new: true });
-//         const populatedTask = await Task.findById(task._id).populate('userId');
-//         if (!task) {
-//             return res.status(404).send({ message: 'Task not found' });
-//         }
-
-//         res.status(200).json(populatedTask);
-//     } catch (error) {
-//         next(error);
-//     }
-// };
-
 
 exports.editTask = async (req, res, next) => {
     const { taskId } = req.params;
-    //COMM: TENGO QUE CAMBIAR EL SET
     try {
-        const task = await Task.findByIdAndUpdate(taskId, { $set: { status: true } }, { new: true })
+        const updatedTask = await Task.findByIdAndUpdate(taskId, req.body, { new: true })
 
-        if (!task) {
+        if (!updatedTask) {
             return res.status(404).send({ message: 'Task not found' });
         }
 
-        res.status(200).json(task);
+        res.status(200).json(updatedTask);
     } catch (error) {
         next(error)
     }
 };
+
