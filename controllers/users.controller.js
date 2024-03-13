4281722a86e212e638f7b7d7c545f43afcc25d5e
paussaanz <paula@sanz.eu>
2024-03-13 20:21:22 +0100

    password update added

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,8 +1,12 @@
 const User = require("../models/User.model");
 const { StatusCodes } = require('http-status-codes');
 const createError = require('http-errors');
+const bcrypt = require('bcrypt');
 
-module.exports.create = (req, res, next) => {
+const ROUNDS = 10;
+
+
+module.exports.createUser = (req, res, next) => {
   const userToCreate = {
     ...req.body,
   }
@@ -12,18 +16,63 @@ module.exports.create = (req, res, next) => {
   }
 
   User.findOne({ email: req.body.email })
-  .then(user => {
-    if (user) {
-      next(createError(StatusCodes.BAD_REQUEST, 'Username or email already in use'));
-    } else {
-      return User.create(userToCreate)
-        .then(userCreated => {
-          res.status(StatusCodes.CREATED).json(userCreated)
-        })
+    .then(user => {
+      if (user) {
+        next(createError(StatusCodes.BAD_REQUEST, 'Username or email already in use'));
+      } else {
+        return User.create(userToCreate)
+          .then(userCreated => {
+            res.status(StatusCodes.CREATED).json(userCreated)
+          })
       }
     })
-  .catch(next)
+    .catch(next)
 
+};
+
+module.exports.updateUser = (req, res, next) => {
+  const { id } = req.params;
+  const userToUpdate = {
+    ...req.body,
+  }
+
+  if (req.file) {
+    userToUpdate.imageUrl = req.file.path;
+  }
+  if (userToUpdate.password) {
+    bcrypt.hash(userToUpdate.password, ROUNDS)
+      .then(hash => {
+        userToUpdate.password = hash;
+        return User.findByIdAndUpdate(id, userToUpdate, { new: true });
+      })
+      .then(updatedUser => {
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found.' });
+        }
+        updatedUser.password = undefined;
+        res.json(updatedUser);
+      })
+      .catch(error => {
+        console.error('Error updating user profile:', error);
+        res.status(500).json({ message: 'Error updating user profile.' });
+      });
+  } else {
+
+    User.findByIdAndUpdate(id, userToUpdate, { new: true })
+    .populate('tasks')
+      .then(updatedUser => {
+        if (!updatedUser) {
+          return res.status(404).send({ message: 'User not found.' });
+        }
+        res.json(updatedUser);
+      })
+      .catch(error => {
+        console.error('Error updating user profile:', error);
+
+        res.status(500).send({ message: 'Error updating user profile.' });
+
+      })
+  }
 };
 
 module.exports.getUser = (req, res, next) => {
@@ -40,6 +89,7 @@ module.exports.getUser = (req, res, next) => {
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.currentUserId)
+    .populate('tasks')
     .then((user) => {
       if (!user) {
         next(createError(402, "User not found"));
